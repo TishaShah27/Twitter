058558cd7e0816ff6ead351ad1ad63f4880a1479
TishaShah27 <152835420+TishaShah27@users.noreply.github.com>
2025-05-25 14:22:05 +0530

    Update firbase.js

--- twiller/src/context/firbase.js
+++ twiller/src/context/firbase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCmNaYQj7yJEgRdO8mD2lgeUlySMTF22pc",
   authDomain: "twillerv2.firebaseapp.com",
   projectId: "twillerv2",
-  storageBucket: "twillerv2.firebasestorage.app",
+  storageBucket: "twillerv2.appspot.com",
   messagingSenderId: "843679836417",
   appId: "1:843679836417:web:8b938b74d0d4da34a4d300",
   measurementId: "G-R1YLD3YKNP"
